a0f7396c3b058f735bda54db1ca35ebacb2fa2a2
Oleg Fedorov <olegajaro94@gmail.com>
2022-06-26 17:32:46 +0700

    Initial Commit

---
+++ PizzaDeliverySwiftUI/PizzaDeliverySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaDeliverySwiftUIApp.swift
+//  PizzaDeliverySwiftUI
+//
+//  Created by Олег Федоров on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaDeliverySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
